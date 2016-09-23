5f336c1b8e6f4bb64a20cace6fc3263d0eb543de
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-09-23 15:54:24 +0100

    Add projectile instantiation

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -7,6 +7,17 @@ public class PlayerController : MonoBehaviour {
 	private float moveSpeed;
 	[SerializeField]
 	private float xClamp;
+	[SerializeField]
+	private GameObject projectile;
+	[SerializeField]
+	private float firingVelocity = 0.5f;
+	[SerializeField]
+	private float firingRate = 0.5f;
+	private bool isShooting = true;
+
+	private void Start () {
+		StartCoroutine (ProjectileShootTimer ());
+	}
 
 	private void Update () {
 		PlayerMovement ();
@@ -21,5 +32,16 @@ public class PlayerController : MonoBehaviour {
 		Vector3 temp = transform.position;
 		temp.x = Mathf.Clamp (temp.x, -xClamp, xClamp);
 		transform.position = temp;
+	}
+
+	private IEnumerator ProjectileShootTimer () {
+		while (isShooting) {
+			yield return new WaitForSeconds (firingRate);
+			InstantiateProjectile ();
+		}
+	}
+
+	private void InstantiateProjectile () {
+		Instantiate (projectile, transform.position, Quaternion.identity);
 	}
 }
