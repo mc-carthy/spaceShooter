cefe5d8141b7bf14fbf642defde730c6b7695498
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-09-23 15:58:18 +0100

    Add projectile movement

---
+++ Assets/scripts/ProjectileController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileController : MonoBehaviour {
+
+	[SerializeField]
+	private float moveSpeed;
+
+	private void Update () {
+		MoveUp ();
+	}
+
+	private void MoveUp () {
+		transform.Translate (new Vector3 (0, moveSpeed, 0));
+	}
+}
