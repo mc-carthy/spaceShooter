e966ad5f45cd8465448bfc79c5f54c91052c9558
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-09-23 15:48:12 +0100

    Add basic horizontal clamped player movement

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	[SerializeField]
+	private float moveSpeed;
+	[SerializeField]
+	private float xClamp;
+
+	private void Update () {
+		PlayerMovement ();
+		ClampXMovement ();
+	}
+
+	private void PlayerMovement () {
+		transform.Translate (new Vector3(Input.GetAxisRaw ("Horizontal"), 0, 0) * moveSpeed);
+	}
+
+	private void ClampXMovement () {
+		Vector3 temp = transform.position;
+		temp.x = Mathf.Clamp (temp.x, -xClamp, xClamp);
+		transform.position = temp;
+	}
+}
