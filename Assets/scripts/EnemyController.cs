b984e07dd6a6583ee83ed8490f9aa4f544130fa2
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-09-23 16:08:43 +0100

    Add enemy spawner and movement

---
+++ Assets/scripts/EnemyController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+
+	[SerializeField]
+	private float moveSpeed;
+
+	private void Update () {
+		MoveDown ();
+	}
+
+	private void MoveDown () {
+		transform.Translate (0, -moveSpeed, 0);
+	}
+}
---
+++ Assets/scripts/EnemySpawner.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject enemyPrefab;
+	[SerializeField]
+	private bool isSpawning = true;
+	[SerializeField]
+	private float spawnRate = 1;
+	[SerializeField]
+	private float xClamp = 24;
+	[SerializeField]
+	private float yMax = 12;
+
+	private void Start () {
+		StartCoroutine(SpawnEnemyTimer ());
+	}
+
+	private IEnumerator SpawnEnemyTimer () {
+		while (isSpawning) {
+			yield return new WaitForSeconds (spawnRate);
+			InstantiateEnemy ();
+		}
+	}
+
+	private void InstantiateEnemy () {
+		Instantiate (
+			enemyPrefab,
+			new Vector3 (Random.Range (-xClamp, xClamp), yMax, 0),
+			Quaternion.identity
+		);
+	}
+}
